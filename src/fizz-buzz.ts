47c1f02236d6e2c3665359409f3732d9481c6f41
Pierre Payet <ext.kleegroup.ppayet@grandlyon.com>
2024-12-31 11:28:25 +0100

    feat: update fizzBuzz method to replace 3 by 'buzz'

--- src/fizz-buzz.ts
+++ src/fizz-buzz.ts
@@ -1,3 +1,4 @@
-export const fizzBuzz = (num: number): Array<number> => {
-    return num >= 2 ? [...Array(num + 1).keys()].slice(1) : [num];
+export const fizzBuzz = (num: number): Array<number | 'buzz'> => {
+    return (num >= 2 ? [...Array(num + 1).keys()].slice(1) : [num])
+        .map(n => n === 3 ? 'buzz': n)
 }
