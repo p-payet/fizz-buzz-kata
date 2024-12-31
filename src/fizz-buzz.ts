5b73ca7f167e61ccea7c514a75ce8bec756713f3
Pierre Payet <ext.kleegroup.ppayet@grandlyon.com>
2024-12-31 11:35:21 +0100

    fix: replace 3 by fizz, and 5 by buzz

--- src/fizz-buzz.ts
+++ src/fizz-buzz.ts
@@ -1,8 +1,8 @@
-export const fizzBuzz = (num: number): Array<number | 'buzz' | 'fizz'> => {
+export const fizzBuzz = (num: number): Array<number | 'fizz' | 'buzz'> => {
     return (num >= 2 ? [...Array(num + 1).keys()].slice(1) : [num])
         .map(n =>
-            n === 3 ? 'buzz'
-                : n === 5 ? 'fizz'
+            n === 3 ? 'fizz'
+                : n === 5 ? 'buzz'
                 : n
         )
 }
--- test/fizz-buzz.spec.ts
+++ test/fizz-buzz.spec.ts
@@ -14,21 +14,21 @@ describe("Fizz Buzz specification tests", () => {
         expect(result).toEqual([1, 2]);
     });
 
-    it("should return 'buzz' instead of 3", () => {
+    it("should return 'fizz' instead of 3", () => {
         const result = fizzBuzz(3);
 
-        expect(result).toEqual([1, 2, 'buzz']);
+        expect(result).toEqual([1, 2, 'fizz']);
     });
 
-    it("should return 'fizz' instead of 5", () => {
+    it("should return 'buzz' instead of 5", () => {
         const result = fizzBuzz(5);
 
-        expect(result).toEqual([1, 2, 'buzz', 4, 'fizz']);
+        expect(result).toEqual([1, 2, 'fizz', 4, 'buzz']);
     });
 
-    it("should return 'buzz' instead of 6", () => {
+    it("should return 'fizz' instead of 6", () => {
         const result = fizzBuzz(6);
 
-        expect(result).toEqual([1, 2, 'buzz', 4, 'fizz', 'buzz']);
+        expect(result).toEqual([1, 2, 'fizz', 4, 'buzz', 'fizz']);
     });
 });
