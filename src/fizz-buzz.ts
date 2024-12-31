983887d1ed20a66ffe78977176b3534e829869f3
Pierre Payet <ext.kleegroup.ppayet@grandlyon.com>
2024-12-31 11:41:44 +0100

    feat: update fizzBuzz method to replace num by 'fizz' if it is divisible by 3

--- src/fizz-buzz.ts
+++ src/fizz-buzz.ts
@@ -1,7 +1,7 @@
 export const fizzBuzz = (num: number): Array<number | 'fizz' | 'buzz'> => {
     return (num >= 2 ? [...Array(num + 1).keys()].slice(1) : [num])
         .map(n =>
-            n === 3 ? 'fizz'
+            n % 3 === 0 ? 'fizz'
                 : n === 5 ? 'buzz'
                 : n
         )
