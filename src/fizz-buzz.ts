b262b7a11e9416b198054f4f1e1aaab552da3055
Pierre Payet <ext.kleegroup.ppayet@grandlyon.com>
2024-12-31 14:56:25 +0100

    refactor: fizzBuzz method

--- src/fizz-buzz.ts
+++ src/fizz-buzz.ts
@@ -3,11 +3,12 @@ export const fizzBuzz = (num: number): Array<number | 'fizz' | 'buzz' | 'fizzbuz
         return [];
     }
 
-    return (num >= 2 ? [...Array(num + 1).keys()].slice(1) : [num])
-        .map(n =>
-            n % 15 === 0 ? 'fizzbuzz'
-                : n % 3 === 0 ? 'fizz'
-                : n % 5 === 0 ? 'buzz'
-                : n
-        );
+    return Array.from({ length: num }, (_, i) => {
+        const n = i + 1;
+
+        return n % 15 === 0 ? 'fizzbuzz'
+            : n % 3 === 0 ? 'fizz'
+            : n % 5 === 0 ? 'buzz'
+            : n;
+    });
 }
