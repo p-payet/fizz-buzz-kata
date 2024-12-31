a138df8ea13edc7e4bc66cfaa26217a83626723e
Pierre Payet <ext.kleegroup.ppayet@grandlyon.com>
2024-12-31 11:54:11 +0100

    test: create a new failing test, it should return 'fizzbuzz' instead of 30

--- test/fizz-buzz.spec.ts
+++ test/fizz-buzz.spec.ts
@@ -43,4 +43,13 @@ describe("Fizz Buzz specification tests", () => {
 
         expect(result).toEqual([1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz']);
     });
+
+    it("should return 'fizzbuzz' instead of 30", () => {
+        const result = fizzBuzz(30);
+
+        expect(result).toEqual([
+            1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz',
+            16, 17, 'fizz', 19, 'buzz', 'fizz', 22, 23, 'fizz', 'buzz', 26, 'fizz', 28, 29, 'fizzbuzz',
+        ]);
+    });
 });
