78c40afd467d2939b5018d11dd41f5f9f1447d5d
Pierre Payet <ext.kleegroup.ppayet@grandlyon.com>
2024-12-31 11:46:46 +0100

    feat: update fizzBuzz method to replace 15 by 'fizzbuzz'

--- src/fizz-buzz.ts
+++ src/fizz-buzz.ts
@@ -1,8 +1,9 @@
-export const fizzBuzz = (num: number): Array<number | 'fizz' | 'buzz'> => {
+export const fizzBuzz = (num: number): Array<number | 'fizz' | 'buzz' | 'fizzbuzz'> => {
     return (num >= 2 ? [...Array(num + 1).keys()].slice(1) : [num])
         .map(n =>
-            n % 3 === 0 ? 'fizz'
+            n === 15 ? 'fizzbuzz'
+                : n % 3 === 0 ? 'fizz'
                 : n % 5 === 0 ? 'buzz'
                 : n
-        )
+        );
 }
