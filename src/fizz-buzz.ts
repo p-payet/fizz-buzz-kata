c50d9d6b1ac4acf07b3b5f480a20626305648e33
Pierre Payet <ext.kleegroup.ppayet@grandlyon.com>
2024-12-31 11:54:53 +0100

    feat: update fizzBuzz method to replace num by 'fizzbuzz' if it is divisible by 15

--- src/fizz-buzz.ts
+++ src/fizz-buzz.ts
@@ -1,7 +1,7 @@
 export const fizzBuzz = (num: number): Array<number | 'fizz' | 'buzz' | 'fizzbuzz'> => {
     return (num >= 2 ? [...Array(num + 1).keys()].slice(1) : [num])
         .map(n =>
-            n === 15 ? 'fizzbuzz'
+            n % 15 === 0 ? 'fizzbuzz'
                 : n % 3 === 0 ? 'fizz'
                 : n % 5 === 0 ? 'buzz'
                 : n
